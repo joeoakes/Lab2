ac551a58b9c459d4cf5f0c431d935b5f4c99a33e
joeoakes <joe.oakes@gmail.com>
2026-02-05 11:10:04 -0500

    javadocs

--- src/monsters/Main.java
+++ src/monsters/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args){
         Monster p1 = new Water("Polly");
         Monster p2 = new Fire("Charzar");
+
+        System.out.println("Pokeman 1 " + p1.getName());
+        System.out.println("Pokeman 2 " + p2.getName());
     }
 
 }
