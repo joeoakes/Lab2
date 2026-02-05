d5ea5ee3a69c077de43a42d5eb5fe26f57ca70c3
joeoakes <joe.oakes@gmail.com>
2026-02-05 11:07:56 -0500

    javadocs

--- src/monsters/Monster.java
+++ src/monsters/Monster.java
@@ -1,4 +1,19 @@
 package monsters;
 
-public class Monster {
+public abstract class Monster {
+
+    public Monster(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
 }
---
+++ src/monsters/Water.java
@@ -0,0 +1,9 @@
+package monsters;
+
+public class Water extends Monster{
+
+
+    public Water(String name) {
+        super(name);
+    }
+}
